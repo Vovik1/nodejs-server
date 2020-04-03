5cc333740fc6331f600d26346b702354dfd22f92
Volodymyr Grytsiv <volodja.grytsiv1996@gmail.com>
2020-04-03 16:02:46 +0300

    Repaired router

--- api/routes/lecture-router.js
+++ api/routes/lecture-router.js
@@ -2,12 +2,10 @@ const express = require('express');
 const router = new express.Router();
 const lectureController = require('../controllers/lecture-controller');
 
-router.get('/', (req,res)=>{
-    res.send(lectureController.getAll);
-});
+router.get('/', lectureController.getAll);
 
-router.post('/create', (req,res)=>{
-    res.send(lectureController.create);
-});
+
+router.post('/create', lectureController.create);
+
 
 module.exports = router;
