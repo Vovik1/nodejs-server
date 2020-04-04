8e02eac682003f60d55925927a2dbe3da58e847d
taras <tarasdida22@gmail.com>
2020-04-04 21:09:41 +0300

    All methods

--- api/routes/lecture-router.js
+++ api/routes/lecture-router.js
@@ -10,6 +10,7 @@ router.post('/create', lectureController.create);
 
 router.put('/update', lectureController.update);
 
+
 router.delete('/delete', lectureController.remove);
 
 module.exports = router;
