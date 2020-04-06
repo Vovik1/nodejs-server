e2ca6fddab9f073bfbeb7da009f9507e8d376fbb
Volodymyr Grytsiv <volodja.grytsiv1996@gmail.com>
2020-04-06 13:48:49 +0300

    Added middleware

--- api/routes/lecture-router.js
+++ api/routes/lecture-router.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = new express.Router();
+const authCheck = require('../middleware/auth-check');
 const lectureController = require('../controllers/lecture-controller');
 
-router.get('/', lectureController.getAll);
+router.get('/', authCheck, lectureController.getAll);
 
-
-router.post('/create', lectureController.create);
+router.post('/create', authCheck, lectureController.create);
 
 
 module.exports = router;
