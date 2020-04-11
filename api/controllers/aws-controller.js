c818473c6b3905e66bebd7b3c02af34651c3a572
Volodymyr Grytsiv <volodja.grytsiv1996@gmail.com>
2020-04-12 02:16:10 +0300

    Second deployment

--- api/controllers/aws-controller.js
+++ api/controllers/aws-controller.js
@@ -7,7 +7,7 @@ const multerS3 = require('multer-s3');
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
 
