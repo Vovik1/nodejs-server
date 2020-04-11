042ae6f37d3a1a79adc96be1afa365cb9a31a500
Volodymyr Grytsiv <volodja.grytsiv1996@gmail.com>
2020-04-12 02:34:55 +0300

    Hroku define port

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const http = require('http');
 const app = require('./app');
 const server = http.createServer(app);
 
-const port = 3030;
+const port = process.env.PORT || 3030;
 server.listen(port, () => console.log(`Server is running on port ${port}`));
