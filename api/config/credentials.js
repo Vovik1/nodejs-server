d1bdce9d263bf61a1f0f47316d8e0d62454fe767
taras <tarasdida22@gmail.com>
2020-04-12 19:57:52 +0300

    reserve commit

--- api/config/credentials.js
+++ api/config/credentials.js
@@ -1,12 +1,12 @@
 module.exports = {
     "facebookAuth":{
-        "clientID": "2372462843064187",
-        "clientSecret": "cad96dc638895011c943020fe0296034",
-        "callbackURL": "http://127.0.0.1:3000/api/auth/facebook/callback"
+        "clientID": "255817578883325",
+        "clientSecret": "de4e3cd2dd6fcbe0dd4ed98655a939a4",
+        "callbackURL": "http://127.0.0.1:3030/api/auth/facebook/callback"
     },
     "googleAuth" : {
         "clientID": "28171160726-tiib31sofoc7lf59qe50e6055cd2ievh.apps.googleusercontent.com",
         "clientSecret": "RSpT5tbv0l_Isga2a1AOMxdI",
-        "callbackURL": "http://localhost:3000/api/auth/google/callback"
+        "callbackURL": "http://localhost:3030/api/auth/google/callback"
     }
 }
--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(morgan('dev'));
 app.use(passport.initialize());
 
 app.use('/api', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
