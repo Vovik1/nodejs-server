285b68b6dcb6cb2fa930a4e3221a03bd20cda00a
taras <tarasdida22@gmail.com>
2020-04-16 13:46:53 +0300

    edit profile token added

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ app.use(passport.initialize());
 
 app.use('/api', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Expose-Headers', 'Access-Token');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   next();
 });
 
