04cddfd45d6f8e703e83816c3031a1232d9d38fe
Taras Dyda <tarasdida22@gmail.com>
2020-05-04 14:08:05 +0300

    webinars begins

--- api/config/socket_io.js
+++ api/config/socket_io.js
@@ -5,7 +5,6 @@ const init = server => {
     const io = socket_io(server);
 
     io.on('connection', socket => {
-        console.log('socket is running!!');
 
         socket.on('join_room', room_id => {
             socket.join(room_id);
@@ -14,6 +13,46 @@ const init = server => {
         socket.on('Leave room', room_id => {
             socket.leave(room_id);
         });
+
+        socket.on('new_webinar_started', webinar_info => {
+            /*webinars.push(webinar_info); */
+            socket.join('Taras');
+        });
+
+        socket.on('add-user', user => {
+            socket.emit('add-user', user);
+        });
+
+        socket.on('remove-user', user => {
+            socket.emit('remove-user', user);
+        });
+
+        socket.on('make-offer', data => {
+            console.log(data);
+            socket.to(data.to).emit('offer-made', {
+                offer: data.offer,
+                socket: socket.id
+            });
+        });
+
+        socket.on('make-answer', data => {
+            socket.emit('answer-made', {
+                socket: socket.id,
+                answer: data.answer
+            });
+        });
+
+        socket.on('stop_webinar', webinar_id => {
+            for(let i=0; i<webinars.length; i++){
+                if(webinars[i].id == webinar_id){
+                    delete webinars[i];
+                }
+            }
+            webinars = webinars.filter(el => {
+                return el != null && el != '' && el != undefined;
+            });
+        });
+
     });
 
     app.io = io;
