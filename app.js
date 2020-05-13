9c916187d4ed3eab4aa1a3f737cd0ea53b23c664
Volodymyr Grytsiv <volodja.grytsiv1996@gmail.com>
2020-05-13 13:33:22 +0300

    Delete Options from Header

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use('/api', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Expose-Headers', 'Access-Token, access-token');
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE');
   next();
 });
 
