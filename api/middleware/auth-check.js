a6c3076bdea327f78f79c3094e58f3c4c456828d
Vovik1 <volodja.grytsiv1996@gmail.com>
2020-05-31 19:11:47 +0300

    Previous auth-check

--- api/middleware/auth-check.js
+++ api/middleware/auth-check.js
@@ -1,28 +1,15 @@
 const jwt = require('jsonwebtoken');
 
-function generateNewJWT (user){
-    const expiry = new Date();
-    expiry.setDate(expiry.getHours() + 1);
-
-    return jwt.sign({
-        _id: user._id,
-        email: user.email,
-        name: user.name,
-        surName: user.surName,
-        role: user.role,
-        exp:parseInt(expiry.getTime()/1000,10)
-    }, process.env.JWT_KEY);
-
-}
-
 module.exports = (req, res, next) => {
-    if (req.headers['access-token']) {
-        jwt.verify(req.headers['access-token'], process.env.JWT_KEY, (err, decoded) => {
-            if (err) return res.sendStatus(401);
-            req.userData = decoded;
-            res.setHeader('Access-Token',generateNewJWT(req.userData));
-            next();
-        });
-    } else res.sendStatus(401);
+  if (req.headers['access-token']) {
+    jwt.verify(
+      req.headers['access-token'],
+      process.env.JWT_KEY,
+      (err, decoded) => {
+        if (err) return res.sendStatus(401);
+        req.userData = decoded;
+        return next();
+      }
+    );
+  } else res.sendStatus(401);
 };
-
