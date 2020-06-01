f16c339e18ab3eb932355f2e2630d7509d236bb3
Vovik1 <volodja.grytsiv1996@gmail.com>
2020-06-01 10:22:56 +0300

    Update lecture-controller.js

--- api/controllers/lecture-controller.js
+++ api/controllers/lecture-controller.js
@@ -166,7 +166,7 @@ function lectureRemove(req, res) {
       if (err) {
         return res.status(404).json(err);
       }
-      res.status(200).json({ message: 'Lecture successfully deleted!' });
+      res.status(204).json(null);
     });
   } else {
     res.status(404).json({ message: 'No Location' });
